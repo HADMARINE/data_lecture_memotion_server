46f5fcbb51a8af9e257759ae86c0096e022cea54
gyu6172 <gyu6172@gmail.com>
2020-11-30 01:39:10 +0900

    MemoList implemented

--- data_lecture_memotion_server/settings.py
+++ data_lecture_memotion_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'data_lecture_memotion_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,10 +77,9 @@ WSGI_APPLICATION = 'data_lecture_memotion_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- memotion/views.py
+++ memotion/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404
+from django.template import loader
+
 
 # Create your views here.
+
+
+from memotion.models import Memo
+
+
+# def login(request):
+#     context = {'user_id': user_id}
+#     return render(request, 'memotion/login.html')
+
+
+def memoindex(request):
+    memo_list = Memo.objects.all().order_by('-pub_date')
+    context = {'memo_list': memo_list}
+    return render(request, 'memotion/memoindex.html', context)
+
+
+def showmemo(request, memo_id):
+    selected_memo = get_object_or_404(Memo, pk=memo_id)
+    return render(request, 'memotion/memo.html', {'selected_memo': selected_memo})
+
+
+# def selectmemo(request, memo_id):
+#     memo_list = get_object_or_404(Post, user_id)
+
