b53a6c6db019d632b1ca4f889ed583e9cb536ae8
HADMARINE <tsx@kakao.com>
2020-11-30 10:53:42 +0900

    update : check descriptions
    1. added requirements.txt
    2. added memotion/urls.py
    3. added middleware - api
    4. update views - wip

--- data_lecture_memotion_server/settings.py
+++ data_lecture_memotion_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,6 +100,25 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = {
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=28),
+}
 
 
 # Internationalization
--- memotion/views.py
+++ memotion/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 
 # Create your views here.
+
+def index(request):
+
+    # if logined, show home.
+    # else, redirect to login.
+
+    id = request.session['token']
+
+
+def login(request):
+    if request.method != "POST":
+        return Http404("Request destination does not exist")
+
+    id = request.POST['id']
+    pw = request.POST['password']
+
+
+
+def logout(request):
+    request.session.clear()
+
+    return render()
+
+def save_post(request):
+
+def delete_post(request):
+
+def update_post(request):
