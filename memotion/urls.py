63b7a1158ee5e48a85dda9d51b7b9a59db816d89
HADMARINE <tsx@kakao.com>
2020-12-05 20:51:00 +0900

    Update project settings

--- memotion/urls.py
+++ memotion/urls.py
@@ -6,14 +6,13 @@ urlpatterns = [
     # path('<str:user_id>/', views.memolist, name='memolist'),
     # path('<str:user_id>/<int:id>/', views.memo, name='memo')
 
-    path('', views.memoindex, name='memoindex'),
-    path('<int:memo_id>/save', views.savememo, name='savememo'),
-    path('<int:memo_id>/', views.showmemo, name='memo'),
-    path('<int:memo_id>/delete', views.deletememo, name='deletememo'),
-    path('newmemo/newmemo', views.newmemo, name="newmemo")
+    path('', views.memoindex, name='index_page'),
+    path('memo/<int:memo_id>/save', views.savememo, name='save_memo'),
+    path('memo/<int:memo_id>/view', views.showmemo, name='show_memo'),
+    path('<int:memo_id>/delete', views.deletememo, name='delete_memo'),
+    path('newmemo/', views.newmemo, name="memo_create"),
     path('login/', login),
     path('logout/', logout),
-    path('post/<int:post_id>/view', get_post),
 ]
 
 app_name = 'memotion'
