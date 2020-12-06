42bccf76098e5800f1866990122305823379fa78
HADMARINE <tsx@kakao.com>
2020-12-06 16:08:50 +0900

    Update settings

--- data_lecture_memotion_server/settings.py
+++ data_lecture_memotion_server/settings.py
@@ -69,6 +69,14 @@ TEMPLATES = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
+
 WSGI_APPLICATION = 'data_lecture_memotion_server.wsgi.application'
 
 
