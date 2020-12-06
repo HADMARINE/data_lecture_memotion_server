9762711af465493f163fe17dbe692e666fa8e9d2
HADMARINE <tsx@kakao.com>
2020-12-06 10:51:36 +0900

    update views

--- memotion/views.py
+++ memotion/views.py
@@ -1,33 +1,33 @@
 import datetime
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
-
-# Create your views here.
-
-
-from memotion.models import Memo
-
-
-# def login(request):
-#     context = {'user_id': user_id}
-#     return render(request, 'memotion/login.html')
+from data_lecture_memotion_server.settings import SECRET_KEY
+from memotion.models import Memo, User
 
 
 def memoindex(request):
+    user_id = request.session.get("user_id", None)
+
+    if user_id is None:
+        # Redirect to login
+        return HttpResponseRedirect("/login")
+
     memo_list = Memo.objects.all().order_by('-pub_date')
     context = {'memo_list': memo_list}
     return render(request, 'memotion/memoindex.html', context)
 
 
-def showmemo(request, memo_id):
+def get_memo(request, memo_id):
     selected_memo = get_object_or_404(Memo, pk=memo_id)
     return render(request, 'memotion/memo.html', {'selected_memo': selected_memo})
 
 
-def savememo(request, memo_id):
+def save_memo(request, memo_id):
     selected_memo = get_object_or_404(Memo, pk=memo_id)
     title = request.POST.get('title', 'wow')
     if title == '':
@@ -42,25 +42,38 @@ def savememo(request, memo_id):
     return HttpResponseRedirect(reverse('memotion:memoindex', args=()))
 
 
-def newmemo(request):
+def create_memo(request):
     memo = Memo()
     memo.title = ""
     memo.content = ""
     memo.pub_date = datetime.datetime.now()
     memo.save()
+
     return render(request, 'memotion/memo.html', {'selected_memo': memo})
 
 
-def deletememo(request, memo_id):
+def delete_memo(request, memo_id):
     Memo.objects.filter(pk=memo_id).delete()
     return HttpResponseRedirect(reverse('memotion:memoindex', args=()))
 
-def index(request):
 
+def index(request):
     # if logined, show home.
     # else, redirect to login.
 
     id = request.session['token']
+
+def create_secret(pw):
+    enc_pw = SECRET_KEY.encrypt(
+        pw,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return enc_pw
 
 
 def login(request):
@@ -68,10 +81,34 @@ def login(request):
         return Http404("Request destination does not exist")
 
     id = request.POST['id']
-    pw = request.POST['password']
+    _pw = request.POST['password']
+    pw = create_secret(_pw)
 
+    user = User.objects.get(user_id=id, password=pw)
+    if user is None:
+        return Http404("Invalid User Data")
 
+    request.session.clear()
+    request.session['user_id'] = id
+    request.session.modified = True
 
 def logout(request):
     request.session.clear()
-    return redirect('/')
+    return HttpResponseRedirect('/login')
+
+def register(request):
+    id = request.POST['id']
+    _pw = request.POST['password']
+    name = request.POST['name']
+
+    find_user = User.objects.get(user_id = id)
+    if find_user is not None:
+        return Http404("User already exists.")
+
+    user = User()
+
+    user.user_id = id
+    user.password = create_secret(_pw)
+    user.name = name
+
+    return HttpResponseRedirect('/login')
